5100341873a9981ce1a92cde05ed23a850a64022
codeoD <david_lee_w@163.com>
2018-12-05 17:23:15 +0800

    样式调整

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -6,12 +6,15 @@
     <ul class="pl-12">
       <li class="text-sm"><router-link to="vueKnowledge" class="hover:no-underline font-bold">Vue Knowledge</router-link></li>
       <ul class="pl-4">
-      <li class="text-xs mt-2"><a href="" class="hover:no-underline">全局配置</a></li>
-      <li class="text-xs mt-2"><a href="" class="hover:no-underline">组件相关</a></li>
-      <li class="text-xs mt-2"><a href="" class="hover:no-underline">Render函数</a></li>
-      <li class="text-xs mt-2"><a href="" class="hover:no-underline">test</a></li>
+        <li class="text-xs mt-2"><a href="" class="hover:no-underline">全局配置</a></li>
+        <li class="text-xs mt-2"><a href="" class="hover:no-underline">组件相关</a></li>
+        <li class="text-xs mt-2"><a href="" class="hover:no-underline">Render函数</a></li>
+        <li class="text-xs mt-2"><a href="" class="hover:no-underline">test</a></li>
       </ul>
       <li class="text-sm mt-2"><a href="" class="hover:no-underline font-bold">Interesting Thing</a></li>
+      <ul class="pl-4">
+        <li class="text-xs mt-2"><a href="">oauth2</a></li>
+      </ul>
     </ul>
   </div>
 </template>
